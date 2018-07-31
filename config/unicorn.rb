7d2ef8c5f095c6ee8ac1139d5e17e36aa0ad5bec
Steven Batcup <steve.batcup@gmail.com>
2018-07-31 14:47:26 +0100

    setup live server

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/hertsjazzfestival/current/"
+pid "/var/www/hertsjazzfestival/current/tmp/pids/unicorn.pid"
+stderr_path "/var/www/hertsjazzfestival/current/log/unicorn.log"
+stdout_path "/var/www/hertsjazzfestival/current/log/unicorn.log"
+
+listen "/tmp/hertsfest_unicorn.sock"
+worker_processes 2
+timeout 120
