bf9e6965d9ca5741f83adbeeb4f55fa0f58d5100
Steven Batcup <steve.batcup@gmail.com>
2018-08-04 19:14:29 +0100

    resort selected items

--- app/fields/performance_field.rb
+++ app/fields/performance_field.rb
@@ -15,15 +15,31 @@ class PerformanceField < Administrate::Field::HasMany
 
   def associated_resource_options
   	musicians = Musician.includes(:instruments).references(:instruments).all
-  	out = []
+  	list = []
+    already_selected_list = {}
     type = associated_class.to_s.downcase
 
   	musicians.each do |musician|
   		musician.instruments.each do |instrument|
-	  		out << ["#{musician.full_name} --> #{instrument.name}", "#{musician.id}_#{instrument.id}"]
+        selected_performance_id = nil
+        data.each do |performance|
+          selected_performance_id = performance.id if performance.musician_id == musician.id && performance.instrument_id == instrument.id
+        end
+        item = ["#{musician.full_name} --> #{instrument.name}", "#{musician.id}_#{instrument.id}"]
+        unless selected_performance_id.nil?
+          already_selected_list[selected_performance_id] = item
+        else
+  	  		list << item
+        end
 	  	end
   	end
-  	out
+    already_selected_list = already_selected_list.sort.reverse.to_h
+
+    already_selected_list.each do |selected_performance|
+      list.unshift selected_performance[1]
+    end
+
+    list
   end
 
 end
