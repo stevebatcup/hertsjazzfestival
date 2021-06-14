33d3e492943d4e5bb09c7b87eaacbe0becd128f6
Steven Batcup <steve.batcup@gmail.com>
2021-06-14 10:43:02 +0100

    Added urrent festival date to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def page_title(text)
+  def page_title(text)
     content_for :page_title, text
   end
 
@@ -7,19 +7,24 @@ module ApplicationHelper
     content_for :"meta_#{tag}", text
   end
 
-  def yield_meta_tag(tag, default_text='')
+  def yield_meta_tag(tag, default_text = "")
     content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
   end
 
-	def gig_price(price)
-		number_to_currency(price, unit: '£')
-	end
+  def gig_price(price)
+    number_to_currency(price, unit: "£")
+  end
 
-	def back_link(gig)
-		if gig.day.festival.is_current? &&  gig.is_film
-			link_to "< Back to films", films_path, id: 'back_to_festival'
-		else
-			link_to "< Back to festival", festival_path(gig.day.festival), id: 'back_to_festival'
-		end
-	end
+  def back_link(gig)
+    if gig.day.festival.is_current? && gig.is_film
+      link_to "< Back to films", films_path, id: "back_to_festival"
+    else
+      link_to "< Back to festival", festival_path(gig.day.festival), id: "back_to_festival"
+    end
+  end
+
+  def festival_date_coverage_sentence(festival)
+    ends = festival.ends.strftime("%d")
+    festival.starts.strftime("%B %d - #{ends}, %Y")
+  end
 end
--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,67 +1,67 @@
 class Festival < ApplicationRecord
-	has_many	:days, dependent: :destroy
-	belongs_to	:venue
-	before_save	:build_dates
+  has_many	:days, dependent: :destroy
+  belongs_to	:venue
+  before_save	:build_dates
 
-	def reverse_days
-		self.days.reverse
-	end
+  def reverse_days
+    days.reverse
+  end
 
-	def gigs
-		@gigs ||= begin
-			gigs = []
-			self.days.each do |day|
-				day.gigs.each do |gig|
-					gigs << gig
-				end
-			end
-			gigs
-		end
-	end
+  def gigs
+    @gigs ||= begin
+      gigs = []
+      days.each do |day|
+        day.gigs.each do |gig|
+          gigs << gig
+        end
+      end
+      gigs
+    end
+  end
 
-	def build_dates
-		self.year = self.starts.strftime("%Y")
-		day_count = ((self.ends - self.starts).to_i / 86400)
-		puts day_count
-		(0..(day_count)).each_with_index do |d, i|
-			day_date = self.starts + d.days
-			self.days.build(date: day_date, sort: i+1) unless Day.find_by(date: day_date, festival_id: self.id)
-		end
-	end
+  def build_dates
+    self.year = starts.strftime("%Y")
+    day_count = ((ends - starts).to_i / 86400)
+    puts day_count
+    (0..day_count).each_with_index do |d, i|
+      day_date = starts + d.days
+      days.build(date: day_date, sort: i + 1) unless Day.find_by(date: day_date, festival_id: id)
+    end
+  end
 
-	def films
-		@films ||= begin
-			film_list = []
-			self.reverse_days.each do |day|
-				day.film_gigs.each do |gig|
-					film_list << gig
-				end
-			end
-			film_list
-		end
-	end
+  def films
+    @films ||= begin
+      film_list = []
+      reverse_days.each do |day|
+        day.film_gigs.each do |gig|
+          film_list << gig
+        end
+      end
+      film_list
+    end
+  end
 
-	def is_in_earlybird_period?
-		if self.earlybird_cutoff_date.nil? || is_in_super_earlybird_period?
-			false
-		else
-			Date.today <= self.earlybird_cutoff_date
-		end
-	end
+  def is_in_earlybird_period?
+    if earlybird_cutoff_date.nil? || is_in_super_earlybird_period?
+      false
+    else
+      Date.today <= earlybird_cutoff_date
+    end
+  end
 
-	def is_in_super_earlybird_period?
-		if self.super_earlybird_cutoff_date.nil?
-			false
-		else
-			Date.today <= self.super_earlybird_cutoff_date
-		end
-	end
+  def is_in_super_earlybird_period?
+    if super_earlybird_cutoff_date.nil?
+      false
+    else
+      Date.today <= super_earlybird_cutoff_date
+    end
+  end
 
-	def earlybird_cutoff_eve
-		self.earlybird_cutoff_date - 1.day
-	end
+  def earlybird_cutoff_eve
+    earlybird_cutoff_date - 1.day
+  end
 
-	def super_earlybird_cutoff_eve
-		self.super_earlybird_cutoff_date - 1.day
-	end
+  def super_earlybird_cutoff_eve
+    super_earlybird_cutoff_date - 1.day
+  end
 end
